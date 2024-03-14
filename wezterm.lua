530bbf6d67a66a8d3dd6128d0fb21e37cb598a9d
Fedoruk Eduard <eduard.fedoruk@nike.com>
2024-03-14 20:30:22 +0200

    change background

---
+++ wezterm.lua
@@ -0,0 +1,15 @@
+local config = {}
+
+config.color_scheme = 'Catppuccin Mocha'
+config.window_background_opacity = 1
+config.hide_tab_bar_if_only_one_tab = true
+config.window_frame = {
+	active_titlebar_bg = '#1e1e2f'
+}
+config.window_background_image = '/Users/EFedo1/.config/wezterm/cosmos-jelly.jpg'
+config.window_background_image_hsb = {
+	brightness = 0.1,
+	hue = 1.0,
+	saturation = 1.0,
+}
+return config
