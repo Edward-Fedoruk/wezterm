63c35355c459051e685d813c18ad6a885412294c
Fedoruk Eduard <eduard.fedoruk@nike.com>
2024-03-18 12:52:42 +0200

    add new bg

--- wezterm.lua
+++ wezterm.lua
@@ -6,9 +6,10 @@ config.hide_tab_bar_if_only_one_tab = true
 config.window_frame = {
 	active_titlebar_bg = '#1e1e2f'
 }
-config.window_background_image = '/Users/EFedo1/.config/wezterm/cosmos-jelly.jpg'
+config.macos_window_background_blur = 100
+config.window_background_image = '/users/efedo1/.config/wezterm/jp-bg-blue.jpg'
 config.window_background_image_hsb = {
-	brightness = 0.1,
+	brightness = 0.05,
 	hue = 1.0,
 	saturation = 1.0,
 }
